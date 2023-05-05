1d3218b89c5c3279f7960191e92af765ed7df9c1
ARYAN RAJ <44833547+AryanRajSaxena@users.noreply.github.com>
2023-05-06 01:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predict():
     x_tt = dataframe.iloc[:, :]
     x_tt_transpose = np.transpose(x_tt)
     result = model.predict(x_tt_transpose)
-    return jsonify({"disease":result})
+    return jsonify({"disease":str(result)})
 
 
 if __name__ == '__main__':
